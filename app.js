c7a5f50899ea4eaffea7a232f48e64045c744558
Shivanshu Singh <62894515+Shivanshu97i@users.noreply.github.com>
2022-12-03 02:54:48 -0800

    Update app.js

--- app.js
+++ app.js
@@ -42,8 +42,13 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 app.get("/secret", (req, res) => {
   res.send("Logged In");
